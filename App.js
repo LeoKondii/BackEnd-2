1079e557a4fe33abdc39e128baae8f38a452c9ea
LeoKondii <conde.leo.feitosa@gmail.com>
2025-11-27 18:06:02 -0300

    Correções do App.js

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
-const routes = require('./Router/Route');
 const express = require('express');
 const app = express();
-const middlewares = require('./Middleware/authenticateToken');
+const routes = require('./Router/Route');
+//const bodyParser = require('body-parser');
+
 //var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 // Setup do Express 
-const path = require('path');
+//const path = require('path');
 //app.use(express.static(path.join(__dirname, 'public'))); // arquivos acessáveis pelo navegador
+
 app.use(session({secret:'segredo', cookie:{maxAge: 60000},resave: true, saveUninitialized: true}));
 //app.use(cookieParser());
 
-
-//const handlebars = require('express-handlebars');
-/*
-app.engine('handlebars', handlebars.engine({defaultLayout: 'NoMenu'}));
-app.set('view engine', 'handlebars');
-*/
-
 app.use(express.json());
+app.use('/api', routes);
 app.use(express.urlencoded({ extended: true }));
 
-app.use(middlewares.logRegister, middlewares.sessionControl)
-app.use(routes);
 
 app.listen(8081, function () {
   console.log("Servidor no http://localhost:8081");
