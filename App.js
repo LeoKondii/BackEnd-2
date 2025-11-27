470fc77c505bfe270702e2cec19a084ce7387578
LeoKondii <conde.leo.feitosa@gmail.com>
2025-11-27 18:26:22 -0300

    Update no App.js

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const app = express();
 const routes = require('./Router/Route');
-//const bodyParser = require('body-parser');
 
-//var cookieParser = require('cookie-parser');
 var session = require('express-session');
 
 // Setup do Express 
 //const path = require('path');
 //app.use(express.static(path.join(__dirname, 'public'))); // arquivos acessáveis pelo navegador
 
-app.use(session({secret:'segredo', cookie:{maxAge: 60000},resave: true, saveUninitialized: true}));
-//app.use(cookieParser());
+app.use(session({secret:'segredo', cookie:{maxAge: 60000}, resave: true, saveUninitialized: true}));
 
 app.use(express.json());
 app.use('/api', routes);
